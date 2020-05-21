4d366afda74a18418e204170ea8666e941ad1703
hvan307 <hongvan307@gmail.com>
2020-05-21 21:03:06 +0100

    Updates

---
+++ precache-manifest.165b6d5a523f4687da20c961eb09566b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e409a5fd287f371f33fbab9044ea0d9",
+    "url": "/PotterPedia/index.html"
+  },
+  {
+    "revision": "d8a137c738207d034b6e",
+    "url": "/PotterPedia/static/css/2.2d597ef9.chunk.css"
+  },
+  {
+    "revision": "5c620040baa92d027430",
+    "url": "/PotterPedia/static/css/main.b326dd6e.chunk.css"
+  },
+  {
+    "revision": "d8a137c738207d034b6e",
+    "url": "/PotterPedia/static/js/2.19a686c9.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/PotterPedia/static/js/2.19a686c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c620040baa92d027430",
+    "url": "/PotterPedia/static/js/main.4d2649dd.chunk.js"
+  },
+  {
+    "revision": "9a8d51c22d47a78b234c",
+    "url": "/PotterPedia/static/js/runtime-main.f6c9cb35.js"
+  },
+  {
+    "revision": "5246eba389dfc5722324b484975a012f",
+    "url": "/PotterPedia/static/media/gryffindor.5246eba3.gif"
+  },
+  {
+    "revision": "ead9b00c180afed4049fee94e64375ee",
+    "url": "/PotterPedia/static/media/houses.ead9b00c.gif"
+  },
+  {
+    "revision": "779fdf5aba6298e4881bc3a9b9db9398",
+    "url": "/PotterPedia/static/media/hufflepuff.779fdf5a.gif"
+  },
+  {
+    "revision": "34e32eadc535b0a67bd16d98efee9a2c",
+    "url": "/PotterPedia/static/media/ravenclaw.34e32ead.gif"
+  },
+  {
+    "revision": "cd6cb76bcba1cbb9fbdd7517b24e06c0",
+    "url": "/PotterPedia/static/media/slytherin.cd6cb76b.gif"
+  }
+]);
